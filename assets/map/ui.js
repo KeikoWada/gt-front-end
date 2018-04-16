b0cb972dd52e4408c6bd41579ada57fd7880d1c2
KeikoWada <kwaddy71385@gmail.com>
2018-04-15 21:32:22 -0400

    Handlebars
    Successfully passing the data and rendering the address
    by using handlebars.
    
    NEXT STEP:
    populate the update forms
    Populate the address in the input field

--- assets/map/ui.js
+++ assets/map/ui.js
@@ -6,11 +6,13 @@ const showMapHtmlTemplate = require('../scripts/templates/map.handlebars')
 const onShowMap = function (data) {
   store.data = data
   console.log(data)
-  const showMapHtml = showMapHtmlTemplate({map: data[0].formatted_address})
-  $('#handlebars').append(showMapHtml)
-  console.log(data[0].address_components[2].long_name)
-  console.log(data[0].formatted_address)
+  // const showMapHtml = showMapHtmlTemplate({map: data[0].formatted_address})
+  // $('#handlebars').append(showMapHtml)
+  // console.log(data[0].address_components[2].long_name)
+  // console.log(data[0].formatted_address)
   // $('#address1').html(data[0].formatted_address)
+  const showMapHtml = showMapHtmlTemplate({map: data})
+  $('#handlebars').append(showMapHtml)
 }
 
 module.exports = {
